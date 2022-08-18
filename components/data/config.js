49ddee3a10221859a5f6e606dab120c9c2955474
faza <arkadyfaza@gmail.com>
2022-08-18 11:02:59 +0700

    add meta description

---
+++ components/data/config.js
@@ -0,0 +1,8 @@
+export const meta = {
+	title: "Capture Inc | Studio Photograpy ",
+	description: "Capture Inc is a Studio Photograpy from Surakarta Jawa Tengah Indonesia",
+	url: "https://google.com",
+	ogDescription: "Capture Inc - Studio Photo, Open Source Enthusiast and Explorer",
+	keywords:
+		"Studio Photograpy,Photography, Studio Photo, Photographer, Wedding Photographer, Photograpy,Surakarta,Jawa Tengah",
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import Meta from "../components/Meta";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
+			<Meta />
 			<Navigation />
 			<Component {...pageProps} />
 			<Footer />
