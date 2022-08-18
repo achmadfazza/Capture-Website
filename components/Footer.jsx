90f73b7e279fa366eb0997c9222a39e89bbd90ec
faza <arkadyfaza@gmail.com>
2022-08-18 11:16:55 +0700

    fiks footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
 	return (
 		<footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6">
 			<span className="text-sm text-gray-500 sm:text-center">
-				<a>Capture&#174;</a>. All Rights Reserved.
+				<p>Capture&#174;</p>. All Rights Reserved.
 			</span>
 			<ul className="flex flex-wrap items-center mt-3 text-sm sm:mt-0">
 				<li>
