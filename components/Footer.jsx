fd9b69c737a394cc44bb5dc4406558d341fb8659
faza <arkadyfaza@gmail.com>
2022-08-19 09:38:40 +0700

    update footer 2

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaGithub, FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 function Footer() {
 	return (
-		<footer className="relative p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 ">
+		<footer className="relative min-h-full sm:mb-[-40px] sm:pb-[40px] flex items-center justify-between p-4 bg-white rounded-lg shadow ">
 			<span className="text-sm text-gray-500 sm:text-center">
 				<a href="/">Capture&#174;</a>. All Rights Reserved.
 			</span>
